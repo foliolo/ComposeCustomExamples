ed9bc72466d5d3f9de1fb61860840dc01caebe06
foliolo <foliolo7891@gmail.com>
2022-09-03 01:17:10 +0100

    Create preview for ItemList

--- app/src/main/java/com/ahgitdevelopment/course/customexamples/ItemList.kt
+++ app/src/main/java/com/ahgitdevelopment/course/customexamples/ItemList.kt
@@ -1,5 +1,6 @@
 package com.ahgitdevelopment.course.customexamples
 
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -10,10 +11,14 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowRight
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ahgitdevelopment.course.customexamples.ui.theme.CustomExamplesTheme
 
 @Composable
 fun ItemList(title: String, onClick: () -> Unit) {
@@ -22,7 +27,8 @@ fun ItemList(title: String, onClick: () -> Unit) {
             .fillMaxWidth()
             .clickable(onClick = onClick)
             .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             text = title,
@@ -35,5 +41,16 @@ fun ItemList(title: String, onClick: () -> Unit) {
             modifier = Modifier.padding(16.dp),
             tint = Color.Black
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreviewItemList() {
+    val context = LocalContext.current
+    CustomExamplesTheme {
+        ItemList(
+            "Item 1",
+            onClick = { Toast.makeText(context, "Element clicked", Toast.LENGTH_SHORT).show() })
     }
 }
